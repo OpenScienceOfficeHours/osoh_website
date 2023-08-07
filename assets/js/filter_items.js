85ed0a441a3dca6d53305d2e4cf3ef578e925e9f
koudyk <kendra.oudyk@gmail.com>
2023-08-07 16:03:30 -0400

    change 'open educational resources' tag to 'educational'

--- assets/js/filter_items.js
+++ assets/js/filter_items.js
@@ -316,8 +316,8 @@ var app = new Vue({
           this.search_tags = ['Open Hardware'];
           this.filterTags();
         }
-        else if (val == 'Open Educational Resources') {
-          this.search_tags = ['Open Educational Resources'];
+        else if (val == 'Educational') {
+          this.search_tags = ['Educational'];
           this.filterTags();
         }
       },
